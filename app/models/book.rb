7ac5093fd569cd1a21fdc5e898ccb7f6485acb0f
ash <satoshi.0411@icloud.com>
2021-03-07 11:53:16 +0900

    バリデーション

--- app/models/book.rb
+++ app/models/book.rb
@@ -6,4 +6,8 @@ class Book < ApplicationRecord
   belongs_to :publisher
   has_many :book_authors
   has_many :authors, through: :book_authors
+
+  validates: name, presence: truer
+  validates: name, length: {maximim: 25}
+  validates: price, numericality: { greater_than_or_equal_to: 0 }
 end
