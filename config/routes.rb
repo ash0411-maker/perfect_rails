3f9243191768f74a8f4bc42ba0c960a8b0be480f
ash <satoshi.0411@icloud.com>
2021-03-07 21:16:49 +0900

    resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :books
   resources :publishers
-  resources :profiles
+  resource :profile, only: %i{show edit update}
   resources :tasks
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
