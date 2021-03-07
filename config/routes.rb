aa2dea8147c1a36be5ec199ca378e74f5c6236f0
ash <satoshi.0411@icloud.com>
2021-03-07 21:15:08 +0900

    Use class

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'books/:id' => "books#show"
-  delete 'books/:id' => "books#destroy"
-  get 'books/index'
-  get 'books/new'
+  resources :books
+  resources :publishers
+  resources :profiles
   resources :tasks
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
