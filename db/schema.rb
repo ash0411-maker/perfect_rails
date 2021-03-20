4c0e02178888a56041fb99a820e5d80bd8abbb7d
ash <satoshi.0411@icloud.com>
2021-03-20 20:11:35 +0900

    [add] AsyncLogモデル

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_07_121358) do
+ActiveRecord::Schema.define(version: 2021_03_20_111015) do
+
+  create_table "async_logs", force: :cascade do |t|
+    t.string "message"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "authors", force: :cascade do |t|
     t.string "name"
