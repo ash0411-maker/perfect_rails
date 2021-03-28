1f546f52cb50e49e24b92ff8383bdb2cfae55e5a
ash <satoshi.0411@icloud.com>
2021-03-28 09:24:33 +0900

    acitive storage 画像表示

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_28_000549) do
+ActiveRecord::Schema.define(version: 2021_03_28_000750) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -75,6 +75,12 @@ ActiveRecord::Schema.define(version: 2021_03_28_000549) do
 
   create_table "tasks", force: :cascade do |t|
     t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
